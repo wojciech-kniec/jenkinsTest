b9af50021c70bfdbb0f587832deeb00ebb48f491
wojciech-kniec <kniecwojciech@gmail.com>
2019-05-10 11:47:59 +0200

    test repaired

--- jenkins_example/src/test/java/pl/stormit/Example1Test.java
+++ jenkins_example/src/test/java/pl/stormit/Example1Test.java
@@ -5,24 +5,24 @@ import org.junit.Test;
 
 public class Example1Test {
 
-	@Test
-	public void test1() {
-	}
+    @Test
+    public void test1() {
+    }
 
-	@Test
-	public void test2() {
-	}
+    @Test
+    public void test2() {
+    }
 
-	@Test
-	public void test3() {
-	}
+    @Test
+    public void test3() {
+    }
 
-	@Test
-	public void test4() {
-	}
+    @Test
+    public void test4() {
+    }
 
-	@Test
-	public void test5() {
-		Assert.fail();
-	}
+    @Test
+    public void test5() {
+//        Assert.fail();
+    }
 }
